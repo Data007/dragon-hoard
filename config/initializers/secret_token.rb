9bd7c613c2c1c2ada6d87e3357a5efbd462aba0c
Michael Christenson II <michael@rebelhold.com>
2011-04-14 20:57:26 -0700

    added base rails 3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DragonHoard::Application.config.secret_token = '61150e9ba16895e9f32987da405cec760d9e2dd82a6e559da767332237f35259a2947251b995157efce2090a43f341af64b160089ebb7da788fae49992383cb2'
