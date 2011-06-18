9b5330873a3e8277e9aa2e2493edaa34ba60935a
Michael Christenson II <michael@rebelhold.com>
2011-06-18 14:25:16 -0400

    coverage for hashing user passwords

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,35 @@
+require 'digest/sha2'
+
 class User
   include Mongoid::Document
   include Mongoid::Timestamps
 
   field :emails, :type => Array, :default => []
   field :phones, :type => Array, :default => []
+  field :login
+  field :password_hash
 
   embeds_many :addresses
+
+  attr_accessor :password, :password_confirmation
+
+  before_save :generate_password_hash
+  
+  def generate_password_hash
+    if (password.present? && password_confirmation.present?)
+      if password == password_confirmation
+        self.password_hash = self.class.send(:hash_password, password)
+      else
+        errors.add :password_confirmation, 'does not match the password'
+      end
+    end
+  end
+
+  class << self
+
+    def hash_password(password)
+      Digest::SHA256.hexdigest password
+    end
+
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,10 @@
 require 'spec_helper'
+require 'digest/sha2'
 
 describe User do
 
-  context 'Embedded Properties' do
+  context 'Contact Information' do
+
     before do
       @user = Factory.create :user
     end
@@ -32,5 +34,44 @@ describe User do
       @user.addresses.create address_hash
       @user.addresses.where(address_hash).should be
     end
+
   end
+
+  context '#hash_password' do
+    
+    it 'hashes password' do
+      User.hash_password('password').should == Digest::SHA256.hexdigest('password')
+    end
+
+    it 'hashes password on save' do
+      user = Factory.build :user, :login => 'test', :password => 'password', :password_confirmation => 'password'
+      user.password_hash.should_not be
+      user.save
+      user.reload.password_hash.should == User.hash_password('password')
+    end
+
+    it 'does not hash on save with a bad password confirmation match' do
+      user = Factory.build :user, :login => 'test', :password => 'password', :password_confirmation => 'passwor'
+      user.save
+      user.errors[:password_confirmation].should be
+      user.reload.password_hash.should_not == User.hash_password('password')
+    end
+
+  end
+
+  context '#authorize' do
+
+    before do
+      @user = Factory.create :user, :login => 'test', :password => 'password', :password_confirmation => 'password'
+    end
+
+    it 'is not active' do
+      pending 'Waiting on #hash_password'
+      User.authorize(@user.login, @user.password).should_not be
+    end
+
+    it 'is active'
+
+  end
+
 end
