b30aaf849a67f779effc542b291009e297ae5a83
Michael Christenson II <michael@rebelhold.com>
2011-07-26 16:58:24 -0400

    forgot some files

---
+++ app/controllers/admin/searches_controller.rb
@@ -0,0 +1,27 @@
+class Admin::SearchesController < AdminController
+  
+  def general
+    @items = Item.search(params[:search][:query]).paginate(pagination_hash)
+
+    redirect_to admin_item_path(@items.first.id) if (@item && @items.length == 1) and return
+
+    @title = "Listing results for: #{params[:search][:query]}"
+    render template: "admin/items/index"
+
+    # if params[:search][:query] =~ /^\d+$/
+    #   @item = Item.where(old_id: params[:search][:query]).first
+    #   redirect_to admin_item_path(@item.id)
+    # elsif params[:search][:query] =~ /^ID\d+\-\d+$/ || params[:search][:query] =~ /^\d+\-\d+$/ || params[:search][:query] =~ /^id\d+\-\d+$/
+    #   item_id, variation_id = params[:search][:query].gsub("ID","").gsub("id","").split("-")
+    #   @item = Item.find(item_id)
+    #   @variation = Variation.find(variation_id)
+    #   redirect_to admin_item_path(:id => @item.id, :variation_id => @variation.id)
+    # else
+    #   @items = Item.search(params[:search][:query]).paginate(pagination_hash)
+    #   @title = "Listing #{params[:search][:query].capitalize} Results"
+    #   @query = params[:search][:query]
+    #   render :template => "admin/items/index"
+    # end  
+  end
+
+end
