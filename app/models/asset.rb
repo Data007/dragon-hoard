1a2d5751da24ed106f8e409b873b895d45573323
Michael Christenson II <michael@rebelhold.com>
2011-08-21 23:36:40 -0700

    different buckets for different environments

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -7,23 +7,23 @@ class Asset
   field :migratory_url
 
   has_mongoid_attached_file :image,
-    :styles => {
-      :tiny       => "100x80",
-      :thumbnail  => "100x120",
-      :small      => "160x120",
-      :medium     => "280x240",
-      :large      => "520x480",
-      :manage     => "340x313"
+    styles: {
+      tiny:      "100x80",
+      thumbnail: "100x120",
+      small:     "160x120",
+      medium:    "280x240",
+      large:     "520x480",
+      manage:    "340x313"
     },
-    :storage => :s3,
-    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
-    :s3_headers => {
-      :content_type => 'application/octet-stream',
-      :content_disposition => 'attachment'
+    storage: :s3,
+    s3_credentials: "#{Rails.root}/config/#{Rails.env}_s3.yml",
+    s3_headers: {
+      content_type:        'application/octet-stream',
+      content_disposition: 'attachment'
     },
-    :s3_protocol => 'https',
-    :path => "variation/:attachment/:id/:basename-:style.:extension",
-    :url => ":s3_domain_url"
+    s3_protocol: 'https',
+    path: "variation/:attachment/:id/:basename-:style.:extension",
+    url:  ":s3_domain_url"
   
   embedded_in :variation
 end
