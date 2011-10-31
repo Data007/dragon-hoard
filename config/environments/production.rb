d3a72c2c086e3c6ff17023696eef3c84308c3f20
Michael Christenson II <michael@rebelhold.com>
2011-10-30 23:02:15 -0700

    ensure staging is in a sandbox for now

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,7 +18,12 @@ end
 
 # ActionController::Base.asset_host     = 'wj-dragon-hoard.heroku.com'
 
-Braintree::Configuration.environment  = :production
-Braintree::Configuration.merchant_id  = "zpgq9twf6gjs6f2v"
-Braintree::Configuration.public_key   = "pnfrrn3nmgksmp9y"
-Braintree::Configuration.private_key  = "rbp5scx345dwfd8y"
+# Braintree::Configuration.environment  = :production
+# Braintree::Configuration.merchant_id  = "zpgq9twf6gjs6f2v"
+# Braintree::Configuration.public_key   = "pnfrrn3nmgksmp9y"
+# Braintree::Configuration.private_key  = "rbp5scx345dwfd8y"
+
+Braintree::Configuration.environment  = :sandbox
+Braintree::Configuration.merchant_id  = "rx4jv5vwyf4z7crh"
+Braintree::Configuration.public_key   = "9q5cqc64w32hmwj4"
+Braintree::Configuration.private_key  = "xkdj42c9qhhzjv7z"
