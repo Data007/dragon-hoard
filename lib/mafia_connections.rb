4c5785564d2dbd8812514a87933697bdde3df2bf
Michael Christenson II <michael@rebelhold.com>
2011-11-15 09:27:36 -0700

    don't parse if valid money

--- lib/mafia_connections.rb
+++ lib/mafia_connections.rb
@@ -1,5 +1,8 @@
 module MafiaConnections
   def launder_money(filthy_lucre)
+    return filthy_lucre if filthy_lucre.match(/[+-]?\d*\.?\d*|\d*/)
+    return 0.0          if filthy_lucre.nil?
+
     laundered_money = filthy_lucre
     %w($ ,).each do |gsub_symbol|
       laundered_money.gsub!("#{gsub_symbol}", '') if laundered_money.match(Regexp.new("\\#{gsub_symbol}"))
@@ -7,6 +10,7 @@ module MafiaConnections
 
     bleached_money = laundered_money.match(/[+-]?\d*\.?\d*|\d*/)
     laundered_money = bleached_money.nil? ? 0 : bleached_money[0]
+
     return laundered_money
   end
 end
