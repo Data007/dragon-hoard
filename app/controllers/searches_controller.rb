7e725e72a6dd62ae7b2609dc36dec7aa15b5c316
Michael Christenson II <michael@rebelhold.com>
2011-11-25 23:58:22 -0700

    added search back to the website

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,19 +1,17 @@
-class Admin::SearchesController < AdminController
-  
-  def general
-    @query = params[:search][:query]
+class SearchesController < ApplicationController
+  def show
+    @query = params[:q]
     @items = Item.search(@query)
     @items = @items.present? ? @items.paginate(pagination_hash) : [].paginate(pagination_hash)
     @title = "Listing #{@query} Results"
 
     if @items.length > 1 || @items.length < 1
-      template = "admin/items/index"
+      template = "items/index"
     else
-      template = "admin/items/show"
+      template = "items/show"
       @item = @items.first
     end
     
     render :template => template
   end
-
 end
