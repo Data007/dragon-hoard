786634065c925b6855cda76efa7aa1dfad64e8e2
Michael Christenson II <michael@rebelhold.com>
2011-11-26 13:11:48 -0700

    just update attributes meta man

--- app/controllers/admin/items/variations_controller.rb
+++ app/controllers/admin/items/variations_controller.rb
@@ -3,7 +3,6 @@ class Admin::Items::VariationsController < Admin::ItemsController
   before_filter :find_variation, except: [:new]
 
   def new
-    debugger
     @variation = @item.variations.create
   end
 
@@ -12,13 +11,10 @@ class Admin::Items::VariationsController < Admin::ItemsController
 
   def update
     params[:variation].delete(:id)
-    params[:variation].each do |(key, value)|
-      eval("@variation.#{key} = '#{value}'")
-    end
 
-    if @variation.save
+    if @variation.update_attributes params[:variation]
       flash[:notice] = 'The variation has been updated'
-      redirect_to edit_admin_item_path(@item.pretty_id)
+      redirect_to edit_admin_item_variation_path(@item.pretty_id, @variation.pretty_id)
     else
       flash[:error] = 'We could not update the variation'
       render action: :edit
