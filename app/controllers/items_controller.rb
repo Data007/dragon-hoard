702442b6f34b76295969dbd43c753edfee6f93e4
Michael Christenson II <michael@rebelhold.com>
2011-11-28 07:36:12 -0700

    fixed variations not loading

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,39 +13,20 @@ class ItemsController < ApplicationController
   end
   
   def show
-    item_id, custom_id = params[:id].match(/^(\d+)(-)?/).captures
+    item_id, custom_id = params[:id].match(/^(\d+)(\S+)?/).captures
 
     begin
       if custom_id
         @item = Item.where(custom_id: item_id.to_i)
-        @item = @item.present? ? @item.first : render(template: 'responses/404') and return
+        @item = @item.first
         @current_variation = params[:variation_id] ? @item.variations.where(custom_id: params[:variation_id].to_i).first : nil
       else
         @item = Item.where(pretty_id: item_id.to_i)
-        @item = @item.present? ? @item.first : render(template: 'responses/404') and return
+        @item = @item.first
         @current_variation = params[:variation_id] ? @item.variations.where(pretty_id: params[:variation_id].to_i).first : nil
       end
     rescue
       render template: 'responses/404'
     end
-
-    # begin
-    #   @item = Item.where(pretty_id: params[:id]).first
-    #   @current_variation = params[:variation_id] ? @item.variations.where(pretty_id: params[:variation_id]).first : nil
-    # rescue
-    #   begin
-    #     @item = Item.where(custom_id: params[:id]).first
-    #     if @item && @item.variations
-    #       @current_variation = params[:variation_id] ? @item.variations.where(pretty_id: params[:variation_id]).first : nil
-    #     else
-    #       render :template => "responses/404"
-    #     end
-    #   rescue
-    #     render :template => "responses/404"
-    #   end
-    # rescue
-    #   render :template => "responses/404"
-    # end
-
   end
 end
