bc33dac14fb7984929682f12e3e8197e39ba6a3d
Michael Christenson II <michael@rebelhold.com>
2011-12-04 14:09:38 -0700

    fixed line_items editing

--- app/controllers/admin/users/orders_controller.rb
+++ app/controllers/admin/users/orders_controller.rb
@@ -65,29 +65,20 @@ class Admin::Users::OrdersController < Admin::UsersController
   
   def update
     params[:order].delete("shipping_option") unless params[:order][:ship]
+    line_items = params[:order].delete(:line_items)
     
     @order = User.find_order(params[:id])
     @order.update_attributes params[:order]
     
-    if params[:order][:line_items].present?
-      params[:order][:line_items].each do |line_item|
+    if line_items.present?
+      line_items.each do |(index, line_item)|
         pretty_id       = line_item[:pretty_id].to_i
         found_line_item = @order.line_items.where(pretty_id: pretty_id).first
 
         line_item.delete(:id)
         line_item.delete(:pretty_id)
-        line_item.reject! {|(key, value)| value.nil?}
-
-        if found_line_item.present?
-          line_item.each do |(key, value)|
-            if (value.match(/^\d*\.*\d*$/) || value.class == TrueClass || value.class == FalseClass)
-              eval("found_line_item.#{key} = #{value}")
-            else
-              eval("found_line_item.#{key} = '#{value}'")
-            end
-          end
-          found_line_item.save
-        end
+        line_item[:taxable] = false unless line_item[:taxable].present?
+        found_line_item.update_attributes line_item
       end
     end
 
