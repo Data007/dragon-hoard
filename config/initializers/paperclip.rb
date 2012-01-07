53d376c0d886c4c019899ad608ae63d708544e79
Michael Christenson II <michael@rebelhold.com>
2012-01-07 11:22:34 -0700

    use new Rails.env

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,2 +1,2 @@
 Paperclip.options[:swallow_stderr] = false
-Paperclip.options[:image_magick_path] = "/usr/local/bin" if RAILS_ENV == "development"
+Paperclip.options[:image_magick_path] = "/usr/local/bin" if Rails.env.development?
