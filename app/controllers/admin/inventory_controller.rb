ff6bc2555ff1b110ece249387e3070eaf632c4c4
Michael Christenson II <michael@rebelhold.com>
2012-01-09 06:56:18 -0700

    restrain by quantity and add quantity and price to report

--- app/controllers/admin/inventory_controller.rb
+++ app/controllers/admin/inventory_controller.rb
@@ -1,6 +1,9 @@
 class Admin::InventoryController < ApplicationController
   def index
-    @variations = Item.available.map(&:variations).flatten.compact.select {|variation| !variation.archived}.flatten.compact
+    @variations = Item.available.map(&:variations)
+      .flatten.compact
+      .select {|variation| variation if !variation.archived && variation.quantity > 0}
+      .flatten.compact
     render layout: false
   end
 end
