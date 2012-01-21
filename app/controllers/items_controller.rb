4392fb504ae4d997a3b00aa26e600bccbaf6b100
Michael Christenson II <michael@rebelhold.com>
2012-01-21 13:39:35 -0700

    removed lookup of old ids

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,19 +13,8 @@ class ItemsController < ApplicationController
   end
   
   def show
-    # TODO: Replace with a normal lookup; kill backwards compatibility with old ids
-    item_id, custom_id = params[:id].match(/^(\d+)(\S+)?/).captures
-
     begin
-      if custom_id
-        @item = Item.where(custom_id: item_id.to_i)
-        @item = @item.first
-        @current_variation = params[:variation_id] ? @item.variations.where(custom_id: params[:variation_id].to_i).first : nil
-      else
-        @item = Item.where(pretty_id: item_id.to_i)
-        @item = @item.first
-        @current_variation = params[:variation_id] ? @item.variations.where(pretty_id: params[:variation_id].to_i).first : nil
-      end
+      @item = Item.where(pretty_id: params[:id]).first
     rescue
       render template: 'responses/404'
     end
