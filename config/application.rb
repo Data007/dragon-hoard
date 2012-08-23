ff56c23ed5d193d36b9c285521058eaaf50e5d02
Michael Christenson II <michael@deepwoodsbrigade.com>
2012-08-23 17:00:50 -0400

    crap

--- config/application.rb
+++ config/application.rb
@@ -31,7 +31,6 @@ module DragonHoardRails32
     config.active_support.escape_html_entities_in_json = true
     config.assets.enabled = true
     config.assets.version = '1.0'
-    config.assets.initialize_on_precompile = false 
   end
 end
 
