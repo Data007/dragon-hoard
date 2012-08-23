641a0f5632326dc7a28cf762ee7984a79f1f24ca
Michael Christenson II <michael@deepwoodsbrigade.com>
2012-08-23 16:30:08 -0400

    using edge moped to try to fix bug #32

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,6 @@
 class PagesController < ApplicationController
   def home
-    # save_previous_page
     @items = Item.published.listable.paginate(pagination_hash)
-  end
-  
-  def about
-    # save_previous_page
   end
   
   def bad_route
