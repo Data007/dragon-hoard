8e6c9f15135772bb939b811d456c39fa696e894c
Michael Christenson II <michael@deepwoodsbrigade.com>
2012-09-09 08:35:31 -0400

    your cart shows

--- spec/requests/dashboard_spec.rb
+++ spec/requests/dashboard_spec.rb
@@ -5,17 +5,28 @@ describe 'Dashboard' do
   before do
     @user  = FactoryGirl.create :web_user
     @item  = FactoryGirl.create :item
-    @asset = @item.assets.create
+    @item.assets.create
+
+    login_with_dh @user.login, 'password'
   end
 
   it 'Shows the dashboard after login' do
-    login_with_dh @user.login, 'password'
-
     current_url.should == url_for([:account])
   end
   
   context 'cart' do
-    it 'shows an item in my cart'
+    it 'shows an item in my cart' do
+      visit '/'
+      click_link @item.name
+      click_link 'Add to Cart'
+      click_link 'Account'
+
+      within '#current-cart' do
+        page.should have_css('.items .item')
+        all('.items .item').count.should == 1
+        page.should have_content(@item.description)
+      end
+    end
   end
   
   context 'orders' do
