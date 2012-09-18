75107a6963dbfb9b3f31515e943f960501d54434
Michael Christenson II <michael@deepwoodsbrigade.com>
2012-09-18 07:55:13 -0400

    added a general "account" layout for users when they are looking at account related actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   before_filter :force_login, :except => [:new, :create, :forgot_password, :generate_new_password]
 
+  layout 'account'
+
   def new
     @user = User.new
   end
