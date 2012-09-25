9b7613fb96b9a8edf228ae041db317684c269d86
Michael Christenson II <michael@deepwoodsbrigade.com>
2012-09-25 08:28:50 -0400

    removed mongoid find hack

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe Item do
-  context '#find' do
-    before do
-      @item = FactoryGirl.create :item
-    end
-
-    it 'finds an item with a pretty id' do
-      Item.find(@item.pretty_id).should == @item
-    end
-
-    it 'finds an item with a bson object id'
-  end
 end
