59b23e5126f6c73a45ce644e3c1967b4f8a3e0b3
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-01-10 09:51:15 -0500

    addded sessions, working on preventing default hres

--- app/assets/javascripts/manage/dashboard.js
+++ app/assets/javascripts/manage/dashboard.js
@@ -1,3 +1,9 @@
 function ManageDashboardController ( $scope, $http, $routeParams ) {
+  $scope.session = {};
 
+  $scope.authenticate = function ( ) {
+    $event.stopPropagation();
+    $event.preventDefault();
+    console.log ( $scope );
+  };
 }
--- app/assets/javascripts/manage/manage.js
+++ app/assets/javascripts/manage/manage.js
@@ -1,9 +1,9 @@
 angular.module ( 'manage', [] ).
   config ( ['$routeProvider', function ( $routeProvider ) {
     $routeProvider.
-      when ( '/home', { templateUrl: '/assets/manage/index.html', controller: 'ManageDashboardController' } ).
-      when ( '/authorize', { templateUrl: '/assets/manage/authorize.html', controller: 'ManageSessionController' } ).
-      when ( '/sales', { templateUrl: '/assets/manage/sales/index.html', controller: 'ManageSalesController' } ).
-      when ( '/sales/new', { templateUrl: '/assets/manage/sales/new.html', controller: 'ManageSalesController' } ).
+      when ( '/home', { templateUrl: '/templates/manage/index.html', controller: 'ManageDashboardController' } ).
+      when ( '/authorize', { templateUrl: '/templates/manage/sessions/new.html', controller: 'ManageSessionController' } ).
+      when ( '/sales', { templateUrl: '/templates/manage/sales/index.html', controller: 'ManageSalesController' } ).
+      when ( '/sales/new', { templateUrl: '/templates/manage/sales/new.html', controller: 'ManageSalesController' } ).
       otherwise ( { redirectTo: '/home' } );
   }]);
