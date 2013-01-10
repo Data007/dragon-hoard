3b2238e042c2436a24005246cf8346226856b788
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-01-10 18:21:39 -0500

    moved javascripts and stylesheets to publid for manage

--- public/javascripts/array.monkeypatch.js
+++
@@ -1,7 +0,0 @@
-Array.prototype.includes = function(value) {
-  var length = this.length;
-  for(var i=0; i < length; i++) {
-    if(this[i] == value) { return true; }
-  }
-  return false;
-}
--- public/javascripts/website/collections/index.js
+++
@@ -1,5 +0,0 @@
-function setup_scrolling(){
-  $(".items.wrapper-pane").pans();
-}
-
-load_functions.push("setup_scrolling");
