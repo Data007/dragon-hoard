2e1e8a604d3bcf0b1370c576691aaec9677afcbe
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-01-10 16:38:29 -0500

    created a sale ...

--- app/assets/javascripts/manage/manage.js
+++ app/assets/javascripts/manage/manage.js
@@ -6,5 +6,6 @@ manageApp.config ( ['$routeProvider', function ( $routeProvider ) {
     when ( '/authorize', { templateUrl: '/templates/manage/sessions/new.html', controller: 'ManageSessionController' } ).
     when ( '/sales', { templateUrl: '/templates/manage/sales/index.html', controller: 'ManageSalesController' } ).
     when ( '/sales/new', { templateUrl: '/templates/manage/sales/new.html', controller: 'ManageSalesController' } ).
+    when ( '/sales/:id', { templateUrl: '/templates/manage/sales/show.html', controller: 'ManageSalesController' } ).
     otherwise ( { redirectTo: '/home' } );
 }]);
--- app/assets/javascripts/manage/sale.js
+++ app/assets/javascripts/manage/sale.js
@@ -1,3 +1,27 @@
-function ManageSalesController ( $scope, $http, $routeParams, $location ) {
+function ManageSalesController ( $scope, $http, $routeParams, $location, $window ) {
   $scope.forceSession ();
+
+  $scope.current_sale = {};
+  $scope.id = $routeParams.id;
+
+  $scope.createSale = function () {
+    if ( $scope.authorized () ) {
+      $http.post ( '/manage/sales', {
+        token: $scope.session.token,
+        order: {
+          created_by: $scope.session.user._id
+        }
+      })
+      .success ( function ( data, status, handlers, config ) {
+        $scope.current_sale = data;
+        $location.path('/sales/' + data.order.pretty_id);
+      })
+      .error ( function ( data, status, handlers, config ) {
+        $scope.flash.error = data.message;
+      });
+    }
+  };
+
+  console.log ( $location.path () );
+  if ( !$scope.id ) $scope.createSale ();
 }
