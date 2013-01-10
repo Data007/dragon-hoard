d6c352d141d8f274c44e08d7dedbf53a3e736175
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-01-10 17:00:18 -0500

    added test angular code back in

---
+++ app/assets/javascripts/angular/hello.js
@@ -0,0 +1,40 @@
+function HelloController($scope, $http) {
+  $scope.session = {authenticated: false, token: ''};
+  $scope.responseText = '';
+  $scope.items  = [];
+  $scope.errors = [];
+
+  $scope.getResponse = function() {
+    $http.get('/angular_tests/hello_angular')
+      .success(function(data, status, handlers, config) {
+        $scope.responseText = data;
+      })
+      .error(function(data, status, handlers, config) {
+        console.log('data: ' + data + ', status: ' + status);
+      });
+  };
+
+  $scope.authenticate = function() {
+    $http.get('/angular_tests/authenticate')
+      .success(function(data, status, handlers, config) {
+        $scope.session = data;
+      })
+      .error(function(data, status, handlers, config) {
+        console.log('data: ' + data + ', status: ' + status);
+      });
+  };
+
+  $scope.getItems = function() {
+    $http.get('/angular_tests/items?token=' + $scope.session.token)
+      .success(function(data, status, handlers, config) {
+        $scope.items = data;;
+      })
+      .error(function(data, status, handlers, config) {
+        if(status == 401){
+          $scope.errors.push(data);
+          console.log($scope.errors);
+        }
+        console.log('data: ' + data + ', status: ' + status);
+      });
+  };
+}
