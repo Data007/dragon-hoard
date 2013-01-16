2a499bdf046b9916af01bf041f82f63e3b7fb35f
Michael Christenson II <michael@deepwoodsbrigade.com>
2013-01-16 09:47:19 -0500

    removed api to revert back to rails views

--- app/controllers/manage/sales_controller.rb
+++ app/controllers/manage/sales_controller.rb
@@ -1,8 +1,2 @@
 class Manage::SalesController < ManageController
-  before_filter :force_access_pin
-
-  def create
-    order = Order.create params[:order]
-    render json: order.to_json(include: [:line_items]), status: order.created_at? ? '200' : '403'
-  end
 end
--- app/controllers/manage_controller.rb
+++ app/controllers/manage_controller.rb
@@ -11,11 +11,4 @@ private
     return nil unless session[:manage_user_id]
     @manage_user ||= User.find(session[:manage_user_id])
   end
-
-  def force_access_pin
-    session = ApiSession.where(token: params[:token]).first
-    unless session
-      render json: {message: 'Unauthorized. You must provide a valid token to access this feature.'}.to_json, status: 401
-    end
-  end
 end
