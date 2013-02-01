04037620ab947f7f0a032640277376e833e78228
lordbyron <bryan@deepwoodsbrigade.com>
2013-02-01 16:15:49 -0500

    almost done with adding new customers

--- spec/requests/manage/customer_management_spec.rb
+++ spec/requests/manage/customer_management_spec.rb
@@ -12,17 +12,18 @@ describe 'Manage Customers' do
     it 'creates a user' do
       click_link 'Add a User'
       current_path.should == new_manage_user_management_path
+      
       soap
       fill_in 'user_first_name', with: 'Bryan'
       fill_in 'user_last_name', with: 'Denslow'
       fill_in 'user_addresses_attributes_0_address_1', with: '2235 S 33 1/2 Rd'
       fill_in 'user_addresses_attributes_0_address_1', with: 'Cadillac'
       fill_in 'user_addresses_attributes_0_address_1', with: '49601'
-      select  'Michigan', from: 'customer_state' 
-      fill_in 'user_phone_number', with: '2318783353'
+      select  'MI', from: 'user_addresses_attributes_0_province' 
+      fill_in 'user_phones_number', with: '2318783353'
       fill_in 'user_email', with: 'bryan@deepwoodsbrigade.com'
       fill_in 'user_anniversary', with: '12/15/12'
-      select  '6.5', from: 'customer_ring_size'
+      select  '6 1/2', from: 'user_ring_size'
       click_button 'Create User'
 
       User.count.should == 2
