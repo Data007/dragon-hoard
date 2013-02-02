0c555c0e1d5eadd4ca467b6ef95adb985ac12af8
lordbyron <bryan@deepwoodsbrigade.com>
2013-02-02 15:03:09 -0500

    can now do crud for users in manage

--- app/controllers/manage/users_controller.rb
+++ app/controllers/manage/users_controller.rb
@@ -1,4 +1,5 @@
 class Manage::UsersController < ApplicationController
+  before_filter :find_customer, except: [:new, :create, :index]
   
   def new
     @customer = User.new
@@ -12,6 +13,25 @@ class Manage::UsersController < ApplicationController
   end
   
   def index
+    @customers = User.where(role: 'customer')
+  end
 
+  def edit
+  end
+
+  def update
+    @customer.update_attributes params[:user]
+    redirect_to manage_users_path, flash: {notice: "Your changes have been saved"}
+  end
+
+  def destroy
+    @customer.destroy
+    redirect_to manage_users_path, flash: {notice: "User has been deleted"}
+  end
+
+private
+  def find_customer
+    customer_id = params[:id].present? ? params[:id] : params[:user]
+    @customer = User.find(customer_id)
   end
 end
--- spec/requests/manage/customer_management_spec.rb
+++ spec/requests/manage/customer_management_spec.rb
@@ -19,17 +19,59 @@ describe 'Manage Customers' do
       fill_in 'user_addresses_attributes_0_city', with: 'Cadillac'
       fill_in 'user_addresses_attributes_0_postal_code', with: '49601'
       select  'MI', from: 'user_addresses_attributes_0_province' 
-      soap
       fill_in 'user_phones_attributes_0_number', with: '2318783353'
       fill_in 'user_email', with: 'bryan@deepwoodsbrigade.com'
       fill_in 'user_email_confirmation', with: 'bryan@deepwoodsbrigade.com'
       fill_in 'user_anniversary', with: '12/15/12'
       select  '6 1/2', from: 'user_ring_size'
-      click_button 'Create User'
+      click_button 'Save'
 
       current_path.should == manage_users_path
 
       User.where(role: 'customer').count.should == 1
+    end
+  end
+
+  context 'with a User' do
+    before do
+      @customer1 = FactoryGirl.create :user, role: 'customer', first_name: 'bryan'
+      @customer2 = FactoryGirl.create :user, role: 'customer', first_name: 'mike'
+      visit manage_path 
+      click_link 'Users'
+    end
+
+    it 'views the customers' do
+      page.should have_content(@customer1.first_name)
+      page.should have_content(@customer1.last_name)
+      page.should have_content(@customer2.first_name)
+      page.should have_content(@customer1.last_name)
+    end
+
+    it 'edits the user' do
+      within("##{@customer1.id}") do
+        click_link 'edit'
+      end
+
+      current_path.should == edit_manage_user_path(@customer1)
+
+      fill_in "user_first_name", with: 'George'
+      fill_in 'user_email', with: 'bryan@hello.com'
+      fill_in 'user_email_confirmation', with: 'bryan@hello.com'
+
+      click_button 'Save'
+      current_path.should == manage_users_path
+
+      User.where(first_name: 'Bryan').count.should == 0
+      User.where(first_name: 'George').count.should == 1
+    end
+
+    it 'deletes a user' do
+      within("##{@customer1.id}") do
+        click_link 'delete'
+      end
+
+      current_path.should == manage_users_path
+      User.where(first_name: 'bryan').count.should == 0
     end
   end
 end
